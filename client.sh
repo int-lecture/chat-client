e77a032c467d14363305e160ed3a6ea087b7f93b
Thomas Smits <thomsmits@gmail.com>
2017-05-12 18:43:17 +0200

    Updated libraries to most current version

--- client.sh
+++ client.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-java -cp lib/jersey-client-1.9.1.jar:lib/jersey-core-1.9.1.jar:lib/jsr311-api-1.1.1.jar:bin/:lib/json-20160810.jar var.chatclient.ChatClient http://localhost:5000 $1 $2
+java -cp lib/jersey-client-1.19.1.jar:lib/jersey-core-1.19.1.jar:lib/jsr311-api-1.1.1.jar:bin/:lib/json-20160810.jar var.chatclient.ChatClient http://localhost:5000 $1 $2
 
